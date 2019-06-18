8edce14aee2f2ddfa3bec55309c03e95586c187e
nevercute <nevercute@icloud.com>
2019-06-18 16:43:21 +0300

    Initial Commit

---
+++ SwiftUIComplexLayout/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUIComplexLayout
+//
+//  Created by NEVERCUTE on 18/06/2019.
+//  Copyright © 2019 NEVERCUTE. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
