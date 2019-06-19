68937583951e7ee818f527addf126e05cecb61af
nevercute <nevercute@icloud.com>
2019-06-19 18:02:20 +0300

    add navbuttons + struct group views

--- SwiftUIComplexLayout/ContentView.swift
+++ SwiftUIComplexLayout/ContentView.swift
@@ -22,6 +22,15 @@ struct ContentView : View {
         .init(id: 3, username: "Daniela Lee", text: "Do you remember that good games? There was awesome, that was some magic and fun. But nature is more realistic and awesome for me", imageName: "paraplan", avatar: "darkwing")
     ]
     
+    let groupItems: [GroupItem] = [
+    .init(id: 0, imageName: "memorial", text: "Georgian Friendship of Peolples's memorial"),
+    .init(id: 1, imageName: "volvo", text: "Brand new car Volvo XC60 released"),
+    .init(id: 2, imageName: "jim", text: "Some awesome sega mega drive game"),
+    .init(id: 3, imageName: "mops", text: "Your pet is waiting for you"),
+    .init(id: 4, imageName: "car", text: "Georgian streets are filled by interesting things"),
+    .init(id: 5, imageName: "paraplan", text: "See a magic views getting by paraplan flight")
+    ]
+    
     var body: some View {
         NavigationView {
             List {
@@ -29,19 +38,14 @@ struct ContentView : View {
                     Text("Trending")
                     ScrollView  {
                         HStack {
-                            Text("Group 1")
-                            Text("Group 2")
-                            Text("Group 3")
-                            Text("Group 4")
-                            Text("Group 5")
-                            Text("Group 1")
-                            Text("Group 2")
-                            Text("Group 3")
-                            Text("Group 4")
-                            Text("Group 5")
+                            ForEach(groupItems.identified(by: \.id)) { item in
+                                NavigationButton(destination: GroupViewDetails(item: item)) {
+                                    GroupView(item: item)
+                                    }.font(.footnote)
+                            }
                         }
                     }
-                    }.frame(height: 150)
+                    }.frame(height: 195)
                 //posts rows
                 ForEach(posts.identified(by: \.id)) { post in
                     PostView(post: post)
@@ -49,6 +53,48 @@ struct ContentView : View {
                 }.navigationBarTitle(Text("Groups"))
         }
     }
+}
+
+struct GroupViewDetails: View {
+    let item: GroupItem
+    var body: some View {
+        VStack (alignment: .center) {
+            Text(item.text)
+                .font(.largeTitle)
+                .lineLimit(nil)
+            Image(item.imageName)
+                .resizable()
+                .scaledToFill()
+                .frame(width: 400, height: 400)
+            .clipped()
+            }
+    }
+}
+
+struct GroupView: View {
+    let item: GroupItem
+    var body: some View {
+        VStack (alignment: .leading) {
+            Image(item.imageName)
+                .renderingMode(.original)
+                .resizable()
+                .scaledToFill()
+                .frame(width: 100, height: 100)
+                .clipped().cornerRadius(12)
+            
+            Text(item.text)
+                .color(.primary)
+                .lineLimit(nil)
+                .font(.subheadline)
+                .padding(.leading, 0)
+            }.frame(width: 100, height: 160)
+            .padding(.trailing, 10)
+    }
+}
+
+struct GroupItem {
+    let id: Int
+    let imageName, text: String
 }
 
 struct PostView: View {
@@ -75,11 +121,11 @@ struct PostView: View {
                 .padding(.trailing, 8)
             Image(post.imageName)
                 .resizable()
-                .frame(height: 360)
+                .frame(width: 414,height: 400)
                 .clipped()
             }.padding(.leading, -20)
             .padding(.bottom, -8)
-        .padding(.trailing, -20)
+            .padding(.trailing, -20)
     }
 }
 
@@ -90,6 +136,8 @@ struct ContentView_Previews : PreviewProvider {
     }
 }
 #endif
+
+
 
 
 
